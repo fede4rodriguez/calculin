593095c05075a787fef557637398de05f0feba20
Fede Rodriguez <tec.federodriguez@gmail.com>
2023-12-14 20:00:15 -0300

    Refactor pressKey function

--- src/hooks/use-calc.ts
+++ src/hooks/use-calc.ts
@@ -1,4 +1,5 @@
 import { useCallback, useState } from "react"
+import { type KeyboardValues, COMMANDS, OPERATORS } from "../lib/constants"
 
 
 export const useCalc = () => {
@@ -6,29 +7,93 @@ export const useCalc = () => {
   const [display, setDisplay] = useState<string>('')
   const [result, setResult] = useState<string>('')
 
-  const pressKey = useCallback((key: string) => {
-    if(key === 'Backspace') {
+  const pressKey = useCallback((keyValue: KeyboardValues) => {
+
+    if(keyValue === COMMANDS['delete']) {
+      setDisplay('')
+      setResult('')
+      // NEW CALC
+      return
+    }
+
+    if(result !== '') {
+
+      if(
+        keyValue === COMMANDS['equal'] ||
+        keyValue === COMMANDS['backspace']        
+      ) {
+        return;
+      }
+
+      // operators
+      if(
+        keyValue === OPERATORS['+'] ||
+        keyValue === OPERATORS['-'] ||
+        keyValue === OPERATORS['*'] ||
+        keyValue === OPERATORS['/']
+      ) {
+        setDisplay(result + keyValue)
+        setResult('')
+        // NEW STEP
+        return
+      }
+
+      
+      // NEW CALC
+      
+      
+      // point
+      if(keyValue === '.') {
+        setResult('')
+        setDisplay('0.')
+        return
+      } 
+      
+      // numbers
+      setResult('')
+      setDisplay(keyValue)  
+      return
+    } 
+
+    // backspace
+    if(keyValue === COMMANDS['backspace']) {
       setDisplay(display.slice(0, -1))
       return
     }
-    if(key === '=') {
-        setResult(eval(display))
-      return
-    }
-      if(key === 'Delete') {
-        setDisplay('')
-        setResult('')
-      return
-    }
-    if(result !== '') {
 
-      const isNumber = !isNaN(Number(key))
-
-      setDisplay(isNumber ? key: result+key)
-      setResult('')
+    // equal
+    if(keyValue === COMMANDS['equal']) {
+      setResult(eval(display))
       return
     }
-    setDisplay(display + key)
+
+    // operators
+    if(
+      keyValue === OPERATORS['+'] ||
+      keyValue === OPERATORS['-'] ||
+      keyValue === OPERATORS['*'] ||
+      keyValue === OPERATORS['/']
+    ) {
+      setDisplay(display + keyValue)
+      return
+    }
+
+    // point
+    if(keyValue === '.') {
+      const lastChar = display.slice(-1)
+      
+      if(lastChar === '') {
+        setDisplay('0.')
+        return
+      }
+
+      const isNumber = !isNaN(parseInt(lastChar))
+      if(!isNumber) return
+    }
+
+    // numbers
+    setDisplay(display + keyValue)
+    
   }, [result, display])  
 
   return { display, result, pressKey }
